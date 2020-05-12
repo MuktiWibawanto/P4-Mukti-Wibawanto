5acd39d935e3e870e5d7f9e13784d5b86eb88943
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:19:12 +0800

    Create Jobsheet 1 C.c
    membuat program array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main (){
+int nim[14]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+printf ("nama : Mukti wibawanto\n");
+printf ("nim : F1B019114\n ");
+printf ("kelompok : 21\n");
+printf ("Data ke-1 = %i\n",nim[0]);
+printf ("Data ke-2 = %i\n",nim[1]);
+printf ("Data ke-3 = %i\n",nim[2]);
+printf ("Data ke-4 = %i\n",nim[3]);
+printf ("Data ke-5 = %i\n",nim[4]);
+printf ("Data ke-6 = %i\n",nim[5]);
+printf ("Data ke-7 = %i\n",nim[6]);
+printf ("Data ke-8 = %i\n",nim[7]);
+printf ("Data ke-9 = %i\n",nim[8]);
+printf ("Data ke-10 = %i\n",nim[9]);
+printf ("Data ke-11 = %i\n",nim[10]);
+printf ("Data ke-12 = %i\n",nim[11]);
+printf ("Data ke-13 = %i\n",nim[12]);
+printf ("Data ke-14 = %i\n",nim[13]);
+}
