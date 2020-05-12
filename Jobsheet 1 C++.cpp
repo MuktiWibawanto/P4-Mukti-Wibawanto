f7e7a0eeaabc3e5eba45a4c977dedf5f81d6a9b6
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:35:45 +0800

    Create Jobsheet 1 C++.cpp
    membuat program array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+using namespace std ;
+
+int main (){
+int myArray[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13,} ;
+
+
+cout << "NAMA :	MUKTI WIBAWANTO \n" ;
+
+cout << "NIM  :	F1B019104 \n"  ;
+
+cout << "Kelompok : 21 \n" ;
+
+cout << endl ;
+
+cout << "Data ke-1 = " <<myArray [0]<<endl ;
+cout << "Data ke-2 = " <<myArray [1]<<endl ;
+cout << "Data ke-3 = " <<myArray [2]<<endl ;
+cout << "Data ke-4 = " <<myArray [3]<<endl ;
+cout << "Data ke-5 = " <<myArray [4]<<endl ;
+cout << "Data ke-6 = " <<myArray [5]<<endl ;
+cout << "Data ke-7 = " <<myArray [6]<<endl ;
+cout << "Data ke-8 = " <<myArray [7]<<endl ;
+cout << "Data ke-9 = " <<myArray [8]<<endl ;
+cout << "Data ke-10 = " <<myArray [9]<<endl ;
+cout << "Data ke-11 = " <<myArray [10]<<endl ;
+cout << "Data ke-12 = " <<myArray [11]<<endl ;
+cout << "Data ke-13 = " <<myArray [12]<<endl ;
+cout << "Data ke-14 = " <<myArray [13]<<endl ;
+}
