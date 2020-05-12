26eb2f0db96b8ff30a4d769e8f6a84ad890d97e8
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:40:21 +0800

    Create Jobsheet 2 C++.cpp
    Membuat array dua dimensi

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout<<"Nama : Mukti wibawanto\n";
+cout<<"Nim : F1B019104\n";
+cout<<"Kelompok: Kelompok 21\n\n";
+int x [14];
+cout<<"masukkan 2 angka NIM terakhir"<<endl;
+cout<<"masukan nim 1 : ";
+cin>>x[0];
+cout<<"masukan nim 2 : ";
+cin>>x[1];
+cout<<"masukan nim 3 : ";
+cin>>x[2];
+cout<<"masukan nim 4 : ";
+cin>>x[3];
+cout<<"masukan nim 5 : ";
+cin>>x[4];
+cout<<"masukan nim 6 : ";
+cin>>x[5];
+cout<<"masukan nim 7 : ";
+cin>>x[6];
+cout<<"masukan nim 8 : ";
+cin>>x[7];
+cout<<"masukan nim 9 : ";
+cin>>x[8];
+cout<<"masukan nim 10 : ";
+cin>>x[9];
+cout<<"masukan nim 11 : ";
+cin>>x[10];
+cout<<"masukan nim 12 : ";
+cin>>x[11];
+cout<<"masukan nim 13 : ";
+cin>>x[12];
+cout<<"masukan nim 14 : ";
+cin>>x[13];
+cout<<endl;
+cout<<"Nim ke-1 = "<<x[0]<<endl;
+cout<<"Nim ke-2 = "<<x[1]<<endl;
+cout<<"Nim ke-3 = "<<x[2]<<endl;
+cout<<"Nim ke-4 = "<<x[3]<<endl;
+cout<<"Nim ke-5 = "<<x[4]<<endl;
+cout<<"Nim ke-6 = "<<x[5]<<endl;
+cout<<"Nim ke-7 = "<<x[6]<<endl;
+cout<<"Nim ke-8 = "<<x[7]<<endl;
+cout<<"Nim ke-9 = "<<x[8]<<endl;
+cout<<"Nim ke-10 = "<<x[9]<<endl;
+cout<<"Nim ke-11 = "<<x[10]<<endl;
+cout<<"Nim ke-12 = "<<x[11]<<endl;
+cout<<"Nim ke-13 = "<<x[12]<<endl;
+cout<<"Nim ke-14 = "<<x[12]<<endl;
+return 0 ;
+}
