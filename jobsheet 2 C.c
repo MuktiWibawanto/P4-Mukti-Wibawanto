51db1b55685d8249e6fa5fc907fe4dc640ede2f8
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:38:59 +0800

    Create jobsheet 2 C.c
    membuat array dua dimensi

---
+++ jobsheet 2 C.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+main()
+{
+	int x[14];
+	printf("masukkan nim mahasiswa ke 1 = ");
+	scanf("%i",&x[0]);
+	printf("masukkan nim mahasiswa ke 2 = ");
+	scanf("%i",&x[1]);
+	printf("masukkan nim mahasiswa ke 3 = ");
+    scanf("%i", &x[2]); 
+	printf("mauskkan nim mahasiswa ke 4 = ");
+	scanf("%i",&x[3]);
+	printf("masukkan nim mahasiswa ke 5 = ");
+	scanf("%i",&x[4]);
+    printf("masukkan nim mahasiswa ke 6 = ");
+	scanf("%i",&x[5]);
+    printf("masukkan nim mahasiswa ke 7 = ");
+	scanf("%i",&x[6]);
+    printf("masukkan nim mahasiswa ke 8 = ");
+	scanf("%i",&x[7]);
+    printf("masukkan nim mahasiswa ke 9 = ");
+	scanf("%i",&x[8]);
+	printf("masukkan nim mahasiswa ke 10 = ");
+	scanf("%i",&x[5]);
+    printf("masukkan nim mahasiswa ke 11 = ");
+	scanf("%i",&x[10]);
+    printf("masukkan nim mahasiswa ke 12 = ");
+	scanf("%i",&x[11]);
+    printf("masukkan nim mahasiswa ke 13 = ");
+	scanf("%i",&x[12]);
+	printf("masukkan nim mahasiswa ke 14 = ");
+	scanf("%i",&x[13]);
+	printf("\n");
+	printf("%i\n",x[0]);
+	printf("%i\n",x[1]);
+	printf("%i\n",x[2]);
+	printf("%i\n",x[3]);
+	printf("%i\n",x[4]);
+	printf("%i\n",x[5]);
+	printf("%i\n",x[6]);
+	printf("%i\n",x[7]);
+	printf("%i\n",x[8]);
+	printf("%i\n",x[9]);
+	printf("%i\n",x[10]);
+	printf("%i\n",x[11]);
+	printf("%i\n",x[12]);
+	printf("%i\n",x[13]);
+	printf("%i\n",x[14]);
+	return 0;
+}
