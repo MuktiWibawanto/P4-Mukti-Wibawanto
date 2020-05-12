2f7baabe5f83eeead5dd7a480553d147c17ae994
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:56:50 +0800

    Create Jobsheet 7 C.c
    program mengisi nilai array dengan perulangan dengan index array dan batas perulangan menggunakan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 7 C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+	int i ;
+	int matriks[14] ;
+	 for (i=1 ; i<=14 ;i++){
+	 	printf ("Masukkan Nilai ke- %d = ",i) ;
+	 	scanf("%i",&matriks[i]) ;
+	 }
+	 printf("\n") ;
+
+}
