e6cc3f5d1ac5efed734666041459f85912b28fd3
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:58:03 +0800

    Create Jobsheet 7 C++.cpp
    program mengisi nilai array dengan perulangan dengan index array dan batas perulangan menggunakan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ; 
+
+int main (){
+	int i ;
+	int matriks[14] ;
+	 for (i=1 ; i<=14 ;i++){
+	 	cout << "Nilai ke-" << i<< " = " ;
+	 	cin >> matriks[i] ;
+	 }
+	 cout <<endl ; 
+}
