927da3f77f8d10e423dbb36f21f6f4ccdba8fbb2
MuktiWibawanto <64573313+MuktiWibawanto@users.noreply.github.com>
2020-05-12 14:59:25 +0800

    Create Jobsheet 8 C.c
    membuat program mengisi nilai array dan menampilkan dengan perulangan index array dan batas pengulangan NIM terakhir setiap Mahasiswa

---
+++ Jobsheet 8 C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+	int i ;
+	int matriks[14] ;
+	printf("Nama :Mukti wibawanto\n");
+    printf("NIM : F1B019104\n");
+	printf("Kelompok :21\n");
+	 for (i=1 ; i<=14 ;i++){
+	 	printf ("Masukkan Nilai ke- %d = ",i) ;
+	 	scanf("%i",&matriks[i]) ;
+	 }
+	 printf("\n") ;
+	 for (i=1 ; i<=14 ;i++){
+	 	printf ("Nilai ke- %i = %d \n ",i,matriks[i]) ;
+}
+printf ("\n") ; }
